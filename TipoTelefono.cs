607fa865cb21772792cdab52d92b89bface78c5b
lucas-propato <lucas.propato.cefe@gmail.com>
2024-05-03 15:25:20 -0300

    Agregar archivos de proyecto.

---
+++ TipoTelefono.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LucasPropato.Prototipos.Ej04
+{
+    internal enum TipoTelefono // no es class, es enum
+    {
+        Casa,
+        Trabajo,
+        Otro
+    }
+}
